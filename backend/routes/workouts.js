8238805b5309d6f3e5d404803d19c03ec3a4d39b
Reander Agulto <reanderagulto29@gmail.com>
2023-04-04 13:29:48 +0800

    Add CRUD Controller

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,36 +1,26 @@
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const {
+    getWorkouts,
+    getWorkout,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
 const router = express.Router()
 
 // GET all Workouts
-router.get('/', (req, res) => {
-    res.json({msg: 'GET all workouts'})
-})
+router.get('/', getWorkouts)
 
 // GET single workout
-router.get('/:id', (req, res) => {
-    res.json({msg: 'GET a single workout'})
-})
+router.get('/:id', getWorkout)
 
 // POST Workout
-router.post('/', async (req, res) => {
-    const { title, load, reps } = req.body
-    try {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-})
+router.post('/', createWorkout)
 
 // DELETE Workout
-router.delete('/:id', (req, res) => {
-    res.json({msg: 'DELETE a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
 // UPDATE Workout
-router.patch('/:id', (req, res) => {
-    res.json({msg: 'UPDATE a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
