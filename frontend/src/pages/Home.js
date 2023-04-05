b790f42c2cecf4eeb1e85ced2abf94bf8d9b1768
Reander Agulto <reanderagulto29@gmail.com>
2023-04-05 22:40:30 +0800

    Connect Frontend to Backend and pull our data from MongoDB

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react"
+
+import WorkoutDetails from '../components/WorkoutDetails'
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts/')
+            const json = await response.json()
+
+            if(response.ok) {
+                setWorkouts(json)
+            }
+        }
+
+        fetchWorkouts()
+    }, [])
+
+    console.log(workouts)
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout, index) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
         </div>
     )
 }
